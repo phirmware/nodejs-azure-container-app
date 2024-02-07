94cd7369022d8c446c3728bfb4621cdfe808383c
majorchork <majorchork@gmail.com>
2024-02-07 12:12:08 -0500

    feat: fix bug

--- app.js
+++ app.js
@@ -38,4 +38,8 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+app.listen(3006, function() {
+  console.log('Server is running on port 3006');
+});
+
 module.exports = app;
