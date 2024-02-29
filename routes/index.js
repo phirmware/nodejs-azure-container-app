5f03b4d96815cae1a388a8482340659ac50930f6
Chibuzor Ifeanyi Ojukwu <chibuzor.ojukwu@gmail.com>
2024-02-29 14:26:28 +0000

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: `Express ${process.env}` });
+  res.render('index', { title: `Express ${process.env.OTEL_EXPORTER_OTLP}` });
 });
 
 module.exports = router;
